17a1deb856de461f61fb71aeb5b4e6f31dde5c26
«Ваше <elena.vakhrusheva92@yandex.ru>
2023-03-20 09:40:24 +0400

    upd

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,17 +1,79 @@
 import React from 'react';
 // import Api from './utils/api.js'
+import { useNavigate } from "react-router-dom";
 
-export default function Dashboard(props) {
-  //const user = getUser();
 
-  const handleLogout = () => {
-    props.history.push('/login');
+const tbl = [
+  {
+    "id": "7546e520-4acc-44b8-bc72-45f5959d9ce5",
+    "documentStatus": "Подписан",
+    "employeeNumber": "1234",
+    "documentType": "Трудовой договор",
+    "documentName": "Договор.pdf",
+    "companySignatureName": "Договор.sig",
+    "employeeSignatureName": "Договор.sig",
+    "employeeSigDate": "2022-12-23T11:19:27.017Z",
+    "companySigDate": "2022-12-23T11:19:27.017Z"
+  },
+  {
+    "id": "b81cbd88-6123-4aea-8c47-5838a62c1d83",
+    "documentStatus": "Подписан",
+    "employeeNumber": "1234",
+    "documentType": "Приказ о приеме",
+    "documentName": "Договор 2.pdf",
+    "companySignatureName": "Приказ.sig",
+    "employeeSignatureName": "Приказ 2.sig",
+    "employeeSigDate": "2022-11-23T11:19:27.017Z",
+    "companySigDate": "2022-11-23T11:19:30.027Z"
   }
+]
+
+export default function Dashboard(setToken, err, isLoading, data) {
+  //const user = getUser();
+  const navigate = useNavigate();
+
+  const handleLogout = e => {
+    e.preventDefault();
+    localStorage.removeItem('userItem');
+    setToken(false);
+  }
+
+
+
+
+  const res = tbl.map(function (item, id) {
+    return <p key={id}>
+      <span>{item.documentStatus}</span>:
+      <span>{item.employeeNumber}</span>
+    </p>;
+  });
 
   return (
     <div>
+      {res}
       Welcome User!<br /><br />
+
       <input type="button" onClick={handleLogout} value="Logout" />
+      <button onClick={() => navigate(-1)}>Go Back</button>
+
+      <div>
+        {err && <h2>{err}</h2>}
+
+        {isLoading && <h2>Loading...</h2>}
+
+        {/* {data.map((d, i) => <Table
+          key={id}
+          companySigDate={d.companyDate}
+          companySignatureName={d.companyName}
+          documentName={d.docName}
+          documentStatus={d.docStatus}
+          documentType={d.docType}
+          employeeNumber={d.empNumer}
+          employeeSigDate={d.empDate}
+          employeeSignatureName={d.empName} />)} */}
+      </div>
+
+
     </div>
   );
 }
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,104 +1,127 @@
 import React, { useState, useEffect } from 'react';
 //import './App.css';
-import { Avatar, Box, Button, Checkbox, Container, createTheme, CssBaseline, FormControlLabel, Grid, Link, TextField, ThemeProvider, Typography } from '@mui/material';
+import { Avatar, Box, Button, Container, CssBaseline, Link, TextField, Typography } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Api from '../../utils/Api';
+import { BrowserRouter } from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard';
 
 export default function Login() {
-  const [data, setData] = useState([]);
-  const [token, setToken] = useState(localStorage.getItem('shop-item'));
+  const [data, setData] = useState({ data: [] });
+  const [token, setToken] = useState(localStorage.getItem('userItem'));
   console.log(token);
   const [api, setApi] = useState(new Api(token));
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
-
-  const handler = e => {
-    e.preventDefault();
-    api.getLogIn({ "username": userName, "password": password })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        setData(data);
-      });
-  }
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState('');
+  const [table, setTable] = useState('');
+  const [TableId, setTableId] = useState(null);
+ 
   useEffect(() => {
     setApi(new Api(token));
   }, [token])
+  
+/*   useEffect(() => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title: 'React Hooks POST Request Example' })
+    };
+    fetch('https://test.v5.pryaniky.com/ru/data/v3/testmethods/docs/userdocs/get', requestOptions)
+      .then(response => response.json())
+      .then(data => setTableId(data.id));
+  }, []); */
 
-  useEffect(() => {
-    fetch("https://test.v5.pryaniky.com/ru/data/v3/testmethods/docs/userdocs/get", {
-      headers: {
-        'x-auth': `${token}`
-      }
-    })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        setData(data);
+  const handleClick = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const response = await fetch('https://test.v5.pryaniky.com/ru/data/v3/testmethods/docs/userdocs/get', {
+        method: 'GET',
+        headers: {
+          'x-auth': `${token}`
+        },
       });
 
-    //.then(data => setTable(data.id));
-  }, [])
- 
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+     
+     const result = await response.json();
+       
+      console.log('result is: ', JSON.stringify(result, null, 4));
+
+      setData(result.id);
+      console.log(setData(data)) 
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <Box component="form" onSubmit={handler} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="userName"
-            label="User Name"
-            name="userName"
-            autoFocus
-            value={userName}
-            onChange={e => setUserName(e.target.value)}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
+    <BrowserRouter>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form"/*  onSubmit={handler}  */ noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="userName"
+              label="User Name"
+              name="userName"
+              autoFocus
+              value={userName}
+              onChange={e => setUserName(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
 
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            token={token}
-            setToken={setToken}
-            api={api}
-            setApi={setApi}
-          >
-            Sign In
-          </Button>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              token={token}
+              setToken={setToken}
+              api={api}
+              setApi={setApi}
+              onClick={handleClick}
+            >
+              <Link to='/Dashboard'>Sign In</Link>
+            </Button>
+          </Box>
         </Box>
-      </Box>
-    </Container>
-
+      </Container>
+    </BrowserRouter>
   );
 }
